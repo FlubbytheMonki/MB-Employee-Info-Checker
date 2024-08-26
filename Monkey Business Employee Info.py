d641bc7e858e4c34beef2bf49119a9c8db97d98b
FlubbytheMonki <pandalover2093@hotmail.com>
2024-08-26 19:56:01 -0400

    Create Monkey Business Employee Info.py

---
+++ Monkey Business Employee Info.py
@@ -0,0 +1,13 @@
+print("Monkey Business Employee Info")
+username = None
+password = None
+flub_username = "monkey"
+flub_password = "monkey"
+flub_todo1 = "eat bananas"
+flub_role = "Role: CEO"
+username = input("What is your Monkey Business username? ")
+password = input("What is your Monkey Business password? ")
+if username == flub_username and password == flub_password:
+    print("Welcome Flub!")
+    print(flub_role)
+    print("To Do's:", flub_todo1)
